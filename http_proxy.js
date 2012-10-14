8e96e72e9e7a73d622ee9839b8f8c2e0f999b0db
Fergal Hainey <fergal@bfot.co.uk>
2012-10-14 13:20:30 +0100

    Turning into filtering proxy
    Example works on 4OD ads.
    Also:
    • http.createClient is deprecated, http.request 
      instead
    • adding picking up port numbers, which also 
      stops some DNS errors

--- http_proxy.js
+++ http_proxy.js
@@ -20,6 +20,16 @@
  THE SOFTWARE.
 */
 
+var rules = [
+  {
+    name : '4OD ads',
+    match : /^http:\/\/ais.channel4.com\/asset\/.+/,
+    filter : function(data) {
+      return data.replace(/<adverts>[\s\S]*<\/adverts>/, '');
+    }
+  }
+];
+
 var sys = require('sys'),
     http = require('http');
 
@@ -27,33 +37,70 @@ var server = http.createServer(function (client_request, client_resp) {
   // sys.puts(sys.inspect(client_request.headers));
 
   var host;
+  var port;
   var path;
 
-  client_request.url.replace(/http:\/\/([^\/]*)(.*)/, function(m, h, p) {
+  client_request.url.replace(/^http:\/\/([^\/:]+)(?::(\d+))?(.*)/, function(m, h, po, pa) {
     host = h;
-    path = p;
+    port = po;
+    path = pa;
   });
+
+  var filtering = false;
+  var rule;
+  for (var i = 0; i < rules.length; i++) {
+    if (rules[i].match.test(client_request.url)) {
+      filtering = true;
+      rule = i;
+      break;
+    }
+  }
 
   path = path || '/';
 
   // sys.puts(client_request.method + " " + host + " " + path);
-  var foreign_host = http.createClient(80, host);
-  var request = foreign_host.request(client_request.method, path, client_request.headers);
-
+  var request = http.request({
+    method : "GET",
+    hostname : host,
+    port: port || 80,
+    path : path,
+    headers : client_request.headers
+  });
+  
+  request.on('error', function(error) {
+    console.log('Request error: '+ error.message, request._headers.host);
+  });
+  
   client_request.addListener("data", function (chunk) {
-    request.sendBody(chunk);
+    request.write(chunk);
   });
 
   client_request.addListener("end", function () {
     request.on('response', function (foreign_response) {
       // sys.puts("STATUS: " + foreign_response.statusCode);
       // sys.puts("HEADERS: " + JSON.stringify(foreign_response.headers));
-
-      client_resp.writeHead(foreign_response.statusCode, foreign_response.headers);
+      
+      client_resp.statusCode = foreign_response.statusCode;
+      for (var name in foreign_response.headers) {
+        client_resp.setHeader(name, foreign_response.headers[name]);
+      }
+      
+      var data = '';
       foreign_response.addListener("data", function (chunk) {
-        client_resp.write(chunk, "binary");
+        if (filtering) {
+          data += chunk;
+        }
+        else {
+          client_resp.write(chunk);
+        }
       });
-      foreign_response.addListener("end", function (chunk) {
+      foreign_response.on('end', function () {
+        if (filtering) {
+          console.log('Filtering ' + rules[rule].name);
+          var newData = rules[rule].filter(data);
+          client_resp.setHeader('Content-length', newData.length);
+          client_resp.write(rules[rule].filter(data));
+        }
         client_resp.end();
       });
     });
